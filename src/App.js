12614531394a743a566fb3eaf66f8abab33c144d
yangshuo <yangshuo@meicai.cn>
2019-08-12 20:45:53 +0800

    test: 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import a, { xuanmiao, xuanmiao2 } from './test/moduleTest'
 
 function App() {
   return (
---
+++ src/test/moduleTest.js
@@ -0,0 +1,6 @@
+let a = {
+  xuanmiao: 1,
+  xuanmiao2: 2
+}
+a.default = a
+module.exports = a
