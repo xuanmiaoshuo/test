161f126de15daf807d95ea8b7f3a8ced4f8cd68a
yangshuo <yangshuo@meicai.cn>
2019-08-22 15:09:08 +0800

    feat: 添加作者

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import a, { xuanmiao, xuanmiao2 } from './test/moduleTest'
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const author = 'xuanmiao'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {author}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
