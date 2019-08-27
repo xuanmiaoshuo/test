92077479c258a6a0a89b67334628b2da101d09eb
yangshuo <yangshuo@meicai.cn>
2019-08-27 09:30:34 +0800

    feat: 添加新的配置

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  // const author = 'xuanmiao'
+  const author = 'xuanmiao'
   return (
     <div className="App">
       <header className="App-header">
